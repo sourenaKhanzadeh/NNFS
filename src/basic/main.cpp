31ed296185674a8e2a68e0d55edb7e512a744b0b
Sourena Khanzadeh <sourena.khanzadeh@gmail.com>
2023-06-14 14:18:51 -0400

    build configured

--- src/basic/main.cpp
+++ src/basic/main.cpp
@@ -4,9 +4,18 @@
 
 
 #include <iostream>
-
+#include "NumCpp.hpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    nc::NdArray<int> a = {1, 2, 3};
+    std::cout << a << std::endl;
+    nc::NdArray<int> b = {4, 5, 6};
+    std::cout << b << std::endl;
+
+    std::cout << a + b << std::endl;
+    std::cout << a * b << std::endl;
+    std::cout << a / b << std::endl;
+    std::cout << a - b << std::endl;
+    std::cout << nc::dot(a, b) << std::endl;
     return 0;
 }
