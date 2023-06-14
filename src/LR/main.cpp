a9fc42265a443c8bf0aa03d207b3261284d9577b
Sourena Khanzadeh <sourena.khanzadeh@gmail.com>
2023-06-13 22:15:15 -0400

    Project folder structured

---
+++ src/LR/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by sourena khanzadeh on 2023-06-13.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World! Linear Regression" << std::endl;
+    return 0;
+}
--- src/basic/main.cpp
+++ src/basic/main.cpp
@@ -1,4 +1,10 @@
+//
+// Created by sourena khanzadeh on 2023-06-13.
+//
+
+
 #include <iostream>
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
